3c6898b4ca77b4188ddd37dc76f1c1cb055fca57
LimitedPark <yuhn1011@naver.com>
2024-01-31 20:23:25 +0900

    Initial commit

---
+++ src/main/java/com/example/gimmegonghakauth/GimmeGonghakAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.gimmegonghakauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GimmeGonghakAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GimmeGonghakAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gimmegonghakauth/GimmeGonghakAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gimmegonghakauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GimmeGonghakAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
