6ab745e6e2387ca069b8a87540b9421d7fbf2f0b
이희수 <hslee9234@naver.com>
2024-02-07 18:57:43 +0900

    SJ-40 기이수 파일 도메인 구현

---
+++ src/main/java/com/example/gimmegonghakauth/domain/ExcelData.java
@@ -0,0 +1,13 @@
+package com.example.gimmegonghakauth.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ExcelData {
+    private Integer student_id; //학번
+    private Integer course_id; //학수번호
+    private Integer year; //수강년도
+    private Integer semester; //수강학기
+}
