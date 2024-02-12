2da538632a61b066d321b33c0ffd6755033597e4
이희수 <hslee9234@naver.com>
2024-02-12 19:29:36 +0900

    SJ-30 기이수 파일 판단 로직 구현
    업로드 하는 파일 용량이 너무 클때의 예외처리가 잘 작동하지 않아 프론트단에서 너무 큰 파일의 업로드를 제한했음

--- src/main/java/com/example/gimmegonghakauth/controller/ExcelController.java
+++ src/main/java/com/example/gimmegonghakauth/controller/ExcelController.java
@@ -1,13 +1,8 @@
 package com.example.gimmegonghakauth.controller;
 
 import com.example.gimmegonghakauth.domain.ExcelDomain;
-import org.apache.commons.io.FilenameUtils;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.DataFormatter;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import com.example.gimmegonghakauth.exception.FileException;
+import com.example.gimmegonghakauth.service.ExcelService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,52 +11,31 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class ExcelController {
 
+    private final ExcelService excelService;
+
+    public ExcelController(ExcelService excelService) {
+        this.excelService = excelService;
+    }
+
     @GetMapping("/excel")
-    public String main(){
+    public String main() {
         return "excel";
     }
 
     @PostMapping("/excel/read")
-    public String readExcel(@RequestParam("file")MultipartFile file, Model model)
-        throws IOException{
-
-        List<ExcelDomain> dataList = new ArrayList<>();
-        String extension = FilenameUtils.getExtension(file.getOriginalFilename());
-
-
-        Workbook workbook = null;
-        if(extension.equals("xlsx")){
-            workbook = new XSSFWorkbook(file.getInputStream());
-        } else if (extension.equals("xls")){
-            workbook = new HSSFWorkbook(file.getInputStream());
+    public String readExcel(@RequestParam("file") MultipartFile file, Model model) {
+        try {
+            List<ExcelDomain> dataList = excelService.extractExcelFile(file);
+            model.addAttribute("datas", dataList);
+            return "excelList";
+        } catch (IOException | FileException e) {
+            model.addAttribute("error", e.getMessage());
+            return "excel";
         }
-
-        Sheet worksheet = workbook.getSheetAt(0);
-        DataFormatter dataFormatter = new DataFormatter();
-
-        for(int i=4; i<worksheet.getPhysicalNumberOfRows(); i++){
-            Row row = worksheet.getRow(i);
-            ExcelDomain data = new ExcelDomain();
-
-            String yearAsString = dataFormatter.formatCellValue(row.getCell(1));
-            data.setYear(Integer.parseInt(yearAsString));
-
-            String semsterAsString = dataFormatter.formatCellValue(row.getCell(2));
-            data.setSemester(Integer.parseInt(String.valueOf(semsterAsString.charAt(0))));
-
-            String course_idAsString = dataFormatter.formatCellValue(row.getCell(3));
-            data.setCourse_id(Integer.parseInt(course_idAsString));
-
-            dataList.add(data);
-        }
-        model.addAttribute("datas",dataList);
-
-        return "excelList";
     }
 }
---
+++ src/main/java/com/example/gimmegonghakauth/service/ExcelService.java
@@ -0,0 +1,84 @@
+package com.example.gimmegonghakauth.service;
+
+import com.example.gimmegonghakauth.domain.ExcelDomain;
+import com.example.gimmegonghakauth.exception.FileException;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ExcelService {
+    public List<ExcelDomain> extractExcelFile(MultipartFile file) throws IOException { //엑셀 데이터 추출
+        List<ExcelDomain> dataList = new ArrayList<>();
+        String extension = FilenameUtils.getExtension(file.getOriginalFilename());
+
+        validateExcelFile(file, extension); //업로드 파일 검증
+
+        Workbook workbook = creatWorkbook(file, extension);
+        Sheet worksheet = workbook.getSheetAt(0);
+        DataFormatter dataFormatter = new DataFormatter();
+
+        validateExcelContent(worksheet, dataFormatter);//엑셀 내용 검증
+
+        for (int i = 4; i < worksheet.getPhysicalNumberOfRows(); i++) { //데이터 추출
+            Row row = worksheet.getRow(i);
+            ExcelDomain data = new ExcelDomain();
+
+            String yearAsString = dataFormatter.formatCellValue(row.getCell(1));
+            data.setYear(Integer.parseInt(yearAsString));
+
+            String semesterAsString = dataFormatter.formatCellValue(row.getCell(2));
+            data.setSemester(Integer.parseInt(String.valueOf(semesterAsString.charAt(0))));
+
+            String course_idAsString = dataFormatter.formatCellValue(row.getCell(3));
+            data.setCourse_id(Integer.parseInt(course_idAsString));
+
+            dataList.add(data);
+        }
+        return dataList; //데이터 반환
+    }
+
+    //업로드 파일 검증
+    private void validateExcelFile(MultipartFile file, String extension) throws FileException {
+        if (file.isEmpty()) { // 파일이 비어있으면
+            throw new FileException("파일이 비어 있습니다.");
+        }
+        if (!extension.equals("xlsx") && !extension.equals("xls")) { //엑셀파일이 아니면
+            throw new FileException("엑셀 파일만 업로드 해주세요.");
+        }
+    }
+
+    // 엑셀 내용 검증
+    private void validateExcelContent(Sheet workSheet, DataFormatter dataFormatter) throws FileException {
+        Row row = workSheet.getRow(0);
+        if (row == null) { //엑셀파일이 비어있으면
+            throw new FileException("엑셀파일이 비어있습니다.");
+        }
+        String data = dataFormatter.formatCellValue(row.getCell(0));
+        if (!data.equals("기이수성적")) { //형식이 올바르지 않으면
+            throw new FileException("기이수성적 엑셀파일을 업로드 해주세요.");
+        }
+    }
+
+    //확장자에 맞춰서 workbook 리턴
+    private Workbook creatWorkbook(MultipartFile file, String extension) throws IOException {
+        Workbook workbook = null;
+        if (extension.equals("xlsx")) {
+            workbook = new XSSFWorkbook(file.getInputStream());
+        } else if (extension.equals("xls")) {
+            workbook = new HSSFWorkbook(file.getInputStream());
+        }
+        return workbook;
+    }
+
+}
