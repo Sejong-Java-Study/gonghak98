73cafd6b7e4679581fe0ed5c0ad1fd655fbe13f2
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2024-05-23 12:46:06 +0900

    Refactor(Domain) : 테이블,컬럼명 확립 및 어노테이션 통일

--- src/main/java/com/example/gimmegonghakauth/domain/AbeekDomain.java
+++ src/main/java/com/example/gimmegonghakauth/domain/AbeekDomain.java
@@ -1,6 +1,7 @@
 package com.example.gimmegonghakauth.domain;
 
 import com.example.gimmegonghakauth.constant.AbeekTypeConst;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -11,20 +12,25 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Range;
 
 @Entity
 @Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "abeek")
 public class AbeekDomain {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "abeek_Id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -33,19 +39,19 @@ public class AbeekDomain {
 
     @NotNull
     @Range(min = 14,max = 24)
+    @Column(name = "year")
     private int year;
 
     @NotNull
     @Enumerated(value = EnumType.ORDINAL)
+    @Column(name = "abeek_type")
     private AbeekTypeConst abeekType;
 
     @NotNull
+    @Column(name = "min_credit")
     private int minCredit;
 
     @Lob
+    @Column(name = "note")
     private String note;
-
-    public AbeekDomain() {
-
-    }
 }
