e94fec628e2048591ca23daa4649e6dc3f03fbcc
이희수 <hslee9234@naver.com>
2024-05-29 14:32:51 +0900

    Refactor: 로그인시 예외처리 추가
    1. 입력값이 null인 경우
    2. 정수입력이 아닌 경우
    3. 사용자가 존재하지않는 경우

--- src/main/java/com/example/gimmegonghakauth/service/UserSecurityService.java
+++ src/main/java/com/example/gimmegonghakauth/service/UserSecurityService.java
@@ -14,6 +14,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+
 
 @RequiredArgsConstructor
 @Service
@@ -21,22 +23,62 @@ public class UserSecurityService implements UserDetailsService {
 
     private final UserDao userDao;
 
+    @Value("${admin1}")
+    private Long admin1;
+
+    @Value("${admin2}")
+    private Long admin2;
+
+    @Value("${admin3}")
+    private Long admin3;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Long studentId = Long.parseLong(username);
-        Optional<UserDomain> _siteUser = this.userDao.findByStudentId(studentId);
-        if (_siteUser.isEmpty()) {
-            throw new UsernameNotFoundException("사용자를 찾을수 없습니다");
+        try {
+            UserDomain siteUser = validateUsername(username);
+            List<GrantedAuthority> authorities = new ArrayList<>();
+            if (checkAdmin(siteUser.getStudentId())) {
+                authorities.add(new SimpleGrantedAuthority(UserRole.ADMIN.getValue()));
+            } else {
+                authorities.add(new SimpleGrantedAuthority(UserRole.USER.getValue()));
+            }
+            return new User(Long.toString(siteUser.getStudentId()), siteUser.getPassword(),
+                authorities);
+        } catch (IllegalArgumentException | UsernameNotFoundException e) {
+            throw new UsernameNotFoundException(e.getMessage(), e); // 발생한 예외를 적절히 처리하여 다시 던짐
         }
-        UserDomain siteUser = _siteUser.get();
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        if (19011684L == studentId) {
-            authorities.add(new SimpleGrantedAuthority(UserRole.ADMIN.getValue()));
-        } else {
-            authorities.add(new SimpleGrantedAuthority(UserRole.USER.getValue()));
-        }
-        return new User(Long.toString(siteUser.getStudentId()), siteUser.getPassword(),
-            authorities);
-
     }
+
+
+    public UserDomain validateUsername(String username)
+        throws IllegalArgumentException, UsernameNotFoundException {
+        // username 이 null 이라면 exception
+        if (username == null) {
+            throw new IllegalArgumentException("학번을 입력해주세요");
+        }
+
+        try {
+            Long studentId = Long.parseLong(username);
+            // parseLong 과정에서 변환할 수 없다면 exception
+            Optional<UserDomain> _siteUser = this.userDao.findByStudentId(studentId);
+            if (_siteUser.isEmpty()) {
+                throw new UsernameNotFoundException("사용자를 찾을 수 없습니다");
+            }
+
+            return _siteUser.get(); // 사용자를 찾았을 때 UserDomain 객체 반환
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("8자리 숫자의 학번을 입력해주세요.");
+        }
+    }
+
+    public Boolean checkAdmin(Long studentId) {
+        
+        if (studentId.equals(admin1) || studentId.equals(admin2) || studentId.equals(admin3)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 }
