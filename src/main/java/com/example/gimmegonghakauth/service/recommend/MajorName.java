0152cb441843217d51a8df62a0d7586f55b193e9
이희수 <hslee9234@naver.com>
2024-08-08 20:48:42 +0900

    refactor: enum MajorName
    '소프트웨어학과', '데이터사이언스학과' 추가

--- src/main/java/com/example/gimmegonghakauth/service/recommend/MajorName.java
+++ src/main/java/com/example/gimmegonghakauth/service/recommend/MajorName.java
@@ -2,7 +2,9 @@ package com.example.gimmegonghakauth.service.recommend;
 
 public enum MajorName {
     ELEC_INFO("전자정보통신공학과"),
-    COMPUTER("컴퓨터공학과");
+    COMPUTER("컴퓨터공학과"),
+    SOFTWARE("소프트웨어학과"),
+    DATA_SCIENCE("데이터사이언스학과");
 
     private final String name;
     MajorName(String name) {
