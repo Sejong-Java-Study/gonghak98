89f9c699b16fcc8c92d300bb6442284a89ae4d9f
이희수 <hslee9234@naver.com>
2024-08-20 16:56:08 +0900

    refactor: InitFileData
    1. 운영용 DB에 동일한 DB 환경 구성
    2. 과목명 띄어쓰기 오류 해결을 위한 CoursesDao.findByNameIgnoreSpaces 메서드 추가
    3. MajorsDao.findById 메서드 추가

--- src/main/java/com/example/gimmegonghakauth/dao/CoursesDao.java
+++ src/main/java/com/example/gimmegonghakauth/dao/CoursesDao.java
@@ -2,6 +2,8 @@ package com.example.gimmegonghakauth.dao;
 
 import com.example.gimmegonghakauth.domain.CoursesDomain;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,4 +12,8 @@ public interface CoursesDao extends JpaRepository<CoursesDomain, Long> {
     CoursesDomain findByCourseId(Long id);
 
     CoursesDomain findByName(String name);
+
+    // 띄워쓰기를 제외한 course.name 과 비교해서 반환하는 쿼리문
+    @Query(value ="select * from course where REPLACE(name, ' ', '') = :name", nativeQuery = true)
+    CoursesDomain findByNameIgnoreSpaces(@Param("name") String name);
 }
