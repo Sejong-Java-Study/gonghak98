8f013776c2543ef296aa9cdd74cd4ea379484411
이희수 <hslee9234@naver.com>
2024-08-24 18:50:02 +0900

    refactor: GonghakCoursesDao
    인증요건 세부항목이 자신의 입학년도를 따르도록 리팩토링

--- src/main/java/com/example/gimmegonghakauth/dao/GonghakCoursesDao.java
+++ src/main/java/com/example/gimmegonghakauth/dao/GonghakCoursesDao.java
@@ -16,8 +16,8 @@ public interface GonghakCoursesDao extends JpaRepository<GonghakCoursesDomain,Lo
 
     @Query("select new com.example.gimmegonghakauth.dto.GonghakCoursesByMajorDto(GCD.coursesDomain.courseId, GCD.coursesDomain.name, GCD.year, GCD.courseCategory, GCD.passCategory, GCD.designCredit, GCD.coursesDomain.credit) from GonghakCoursesDomain GCD "
         + "join CompletedCoursesDomain CCD on GCD.coursesDomain = CCD.coursesDomain "
-        + "where CCD.userDomain.studentId =:studentId and GCD.majorsDomain.id = :majorsId and CCD.year = GCD.year")
-    List<GonghakCoursesByMajorDto> findUserCompletedCourses(@Param("studentId") Long studentId, @Param("majorsId") Long majorId);
+        + "where CCD.userDomain.studentId =:studentId and GCD.majorsDomain.id = :majorsId and GCD.year = :year")
+    List<GonghakCoursesByMajorDto> findUserCompletedCourses(@Param("studentId") Long studentId, @Param("majorsId") Long majorId, @Param("year") Long year);
 
     @Query("select new com.example.gimmegonghakauth.dto.IncompletedCoursesDto(GCD.coursesDomain.name, GCD.courseCategory, GCD.coursesDomain.credit, GCD.designCredit) from GonghakCoursesDomain GCD  "
         + "left join CompletedCoursesDomain CCD on CCD.coursesDomain = GCD.coursesDomain "
