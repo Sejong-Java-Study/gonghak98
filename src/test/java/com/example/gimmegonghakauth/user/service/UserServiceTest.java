c2238170bdbbb69574de97f86ede259bbcdc671a
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2024-10-06 15:27:28 +0900

    test: UserService 테스트 코드 추가
    - getByStudentId 테스트

--- src/test/java/com/example/gimmegonghakauth/user/service/UserServiceTest.java
+++ src/test/java/com/example/gimmegonghakauth/user/service/UserServiceTest.java
@@ -1,9 +1,10 @@
 package com.example.gimmegonghakauth.user.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.example.gimmegonghakauth.exception.UserNotFoundException;
 import com.example.gimmegonghakauth.user.domain.UserDomain;
-import com.example.gimmegonghakauth.user.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -44,4 +45,31 @@ class UserServiceTest {
         //then
         assertThat(updatedUser.getPassword()).isEqualTo("Fake" + newPassword);
     }
+
+    @Test
+    void 사용자의_학번으로_User를_가져올_수_있다() {
+        //given
+        UserDomain user = userService.create(String.valueOf(id), password, email, null, name);
+
+        //when
+        UserDomain findUser = userService.getByStudentId(id);
+
+        //then
+        assertThat(findUser.getStudentId()).isEqualTo(user.getStudentId());
+        assertThat(findUser.getEmail()).isEqualTo(user.getEmail());
+        assertThat(findUser.getName()).isEqualTo(user.getName());
+    }
+
+    @Test
+    void 사용자의_학번으로_User를_찾지_못하면_예외가_발생한다() {
+        //given
+        UserDomain user = userService.create(String.valueOf(id), password, email, null, name);
+        Long wrongId = 10000000L;
+
+        //when & then
+        assertThatThrownBy(() -> userService.getByStudentId(wrongId))
+            .isInstanceOf(UserNotFoundException.class);
+    }
+
+
 }
